052c2375b37da046b88a3f18c989163409bcff14
nonsecularman <krishbharti3404@gmail.com>
2026-01-16 23:00:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/devilcode-53/kryshMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/nonsecularman/kryshMusic?tab=readme-ov-file")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
